4c3bffc351980c619600cae3d193321b9ef46c0d
Pluto Yang <yangyj.ee@gmail.com>
2023-06-29 11:07:06 +0800

    增加缺少的几个 python 库

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ apt-get update
 apt-get -y upgrade
 
 apt-get -y install python3-opencv python3-willow python3-pyaudio python3-sklearn swi-prolog vim
-apt-get -y install python3-picamera2 python3-torch python3-torchvision
-pip3 install python_speech_features pyswip 
+apt-get -y install python3-picamera2 python3-torch python3-torchvision python3-soundfile
+pip3 install python_speech_features pyswip sounddevice hmmlearn
 
